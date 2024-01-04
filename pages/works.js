687e497193a7d6d33021a91b30842eea9424bbc4
AxewBoTX <lovedeepbrar54155@gmail.com>
2024-01-04 19:39:45 +0530

    Cleaning some stuff up

--- pages/works.js
+++ pages/works.js
@@ -15,27 +15,15 @@ function Works() {
             </Heading>
           </Section>
           <SimpleGrid columns={[1, 2, 2]} gap="6" mt="50px">
-            <GridItem
-              title="The only manager you will ever need"
-              thumbnail="https://raw.githubusercontent.com/AxewBoTX/dondu/main/static/dondu-project.png"
-              href="https://dondu.netlify.app/"
-            >
-              Dondu
-            </GridItem>
-            <GridItem
-              title="It's Just A Calculator"
-              thumbnail="/calci.png"
-              href="https://calci.is-an.app/"
-            >
-              Calci
-            </GridItem>
-            <GridItem
-              title="Better Social Life"
-              thumbnail="/socio.png"
-              href="https://socio-web.netlify.app/"
-            >
-              Socio
-            </GridItem>
+            {/*
+				<GridItem
+				title="--title--"
+				thumbnail="--thumbnail-picture--"
+				href="--link--"
+				>
+				--Name--
+				</GridItem>
+			*/}
           </SimpleGrid>
         </Box>
       </Layout>
