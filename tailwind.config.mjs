ae4028ab21b0c8fd8b243e29bc9d288f8d40176d
AxewBoTX <lovedeepbrar54155@gmail.com>
2024-01-25 16:43:13 +0530

    DaisyUI

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -4,5 +4,33 @@ export default {
   theme: {
     extend: {},
   },
-  plugins: [require("@tailwindcss/typography")],
+  plugins: [require("@tailwindcss/typography"), require("daisyui")],
+  daisyui: {
+    themes: [
+      {
+        dark: {
+          primary: "#f78265",
+          secondary: "#1ec99e",
+          accent: "#2573a3",
+          neutral: "#20303c",
+          "base-100": "#353437",
+          info: "#8bc6f4",
+          success: "#45d3c0",
+          warning: "#f8cf68",
+          error: "#f53848",
+        },
+        light: {
+          primary: "#50e0c8",
+          secondary: "#f49cd5",
+          accent: "#cd8fea",
+          neutral: "#1e1727",
+          "base-100": "#f6f2f8",
+          info: "#92a7e7",
+          success: "#73deaf",
+          warning: "#e2aa03",
+          error: "#f36d93",
+        },
+      },
+    ],
+  },
 };
