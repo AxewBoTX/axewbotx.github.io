3ab427a6165176eed73ec7ecc25a914b1197d732
AxewBoTX <lovedeepbrar54155@gmail.com>
2024-01-25 07:51:19 +0530

    Basic Vite App

--- src/lib/stores.js
+++
@@ -1,3 +0,0 @@
-import { writable } from "svelte/store";
-
-export const AppTheme = writable("dark");
---
+++ src/main.js
@@ -0,0 +1,5 @@
+document.querySelector("#app").innerHTML = `
+  <div>
+	<h1 class="text-3xl">Hello, Bro!</h1>
+  </div>
+`;
