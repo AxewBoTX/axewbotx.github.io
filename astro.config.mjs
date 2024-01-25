f23fe17919f5cbe7bca4f87e652749355fe6a606
AxewBoTX <lovedeepbrar54155@gmail.com>
2024-01-25 16:43:02 +0530

    AstroIcon integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,13 @@
 import { defineConfig } from "astro/config";
-
 import tailwind from "@astrojs/tailwind";
+
+import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
   server: {
     port: 3000,
   },
-  integrations: [tailwind()],
+  integrations: [tailwind(), icon()],
 });
 
