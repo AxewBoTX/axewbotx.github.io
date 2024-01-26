6b726fa07b92672d3bbd68a20381b841a132783d
AxewBoTX <lovedeepbrar54155@gmail.com>
2024-01-26 18:33:26 +0530

    mdx integration
    integrating mdx to astro and configuring it to use the shiki syntax highlighting plugin and set the syntax theme to vscode 'dark-plus'

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,20 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-
 import icon from "astro-icon";
+
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
   server: {
     port: 3000,
   },
-  integrations: [tailwind(), icon()],
+  integrations: [
+    tailwind(),
+    icon(),
+    mdx({
+      syntaxHighlight: "shiki",
+      shikiConfig: { theme: "dark-plus" },
+    }),
+  ],
 });
-
